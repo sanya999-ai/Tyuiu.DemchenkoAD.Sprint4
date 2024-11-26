df4448d4b832bb25a7563435e98c763a4c513ce5
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-11-26 10:33:20 +0500

    Add project files.

---
+++ Tyuiu.DemchenkoAD.Sprint4.Task0.V29.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.DemchenkoAD.Sprint4.Task0.V29.Lib
+{
+    public class DataService : ISprint4Task0V29
+    {
+        public int GetSumOddArrEl(int[] array)
+        {
+            int res = 0;
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    res += array[i];
+                }
+            }
+            return res;
+        }
+    }
+}
