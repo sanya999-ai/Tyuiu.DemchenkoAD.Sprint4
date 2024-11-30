35f2bbba6fbd698497dc8b7e8b27968900319db6
Александра Демченко <Tyuiu.DemchenkoAD@outlook.com>
2024-11-30 14:19:25 +0500

    Task3

---
+++ Tyuiu.DemchenkoAD.Sprint4.Task3.V4.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.DemchenkoAD.Sprint4.Task3.V4.Lib
+{
+    public class DataService : ISprint4Task3V4
+    {
+        public int Calculate(int[,] array)
+        {
+            int rows = array.GetUpperBound(0) + 1; // кол-во строк
+            int columns = array.Length / rows;   // кол-во столбцов 
+            int minim = 9;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 4; j < columns; j++)
+                {
+                    if (array[i, j] < minim)
+                    {
+                        minim = array[i, j];
+                    }
+                }
+            }
+            return minim;
+        }
+    }
+}
---
+++ Tyuiu.DemchenkoAD.Sprint4.Task3.V4/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.DemchenkoAD.Sprint4.Task3.V4.Lib;
+namespace Tyuiu.DemchenkoAD.Sprint4.Task3.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнила: Демченко А. Д. | ИСПб-24-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы. (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Демченко Александра | ИСПб-24-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найти минимальный элемент в последнем столбце массива.                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Массив:");
+
+            int[,] mas2 = new int[5, 5] {  { 6, 5, 4, 1, 5 },
+                                           { 8, 2, 3, 4, 2 },
+                                           { 3, 7, 7, 1, 3 },
+                                           { 3, 4, 8, 1, 3 },
+                                           { 4, 3, 5, 5, 2 } };
+
+            int rows = mas2.GetUpperBound(0) + 1;
+            int columns = mas2.Length / rows;
+            Console.WriteLine("Массив: ");
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 4; j < columns; j++)
+                {
+                    Console.WriteLine($"{mas2[i, j]} \t");
+                }
+
+
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            int res = ds.Calculate(mas2);
+            Console.WriteLine("Минимальный элемент в последнем столбце массива " + res);
+            Console.ReadKey();
+        }
+    }
+}
